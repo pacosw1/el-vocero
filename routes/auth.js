e73a06fc73c8bcb98f633eb80ff5d7710c79cadd
Paco <pacoswilliams@gmail.com>
2019-05-01 13:30:36 -0500

    add fix

--- routes/auth.js
+++ routes/auth.js
@@ -6,21 +6,20 @@ require("dotenv");
 
 const auth = async (req, res) => {
   let { username, password } = req.body;
+  let notFound = { status: 0, message: "invalid username or password" };
 
   console.log(req.body);
   let user = await db.findOne({ username: username });
   console.log(user);
-  if (!user) res.send("no");
+  if (!user) res.send(notFound);
 
   //input validation
-  if (!username || !password)
-    return res.send({ status: 0, message: "invalid username or password" });
+  if (!username || !password) return res.send(notFound);
   //wtf
   //verify password
   let validPassword = await bcrypt.compare(password, user.password);
   console.log(validPassword);
-  if (!validPassword)
-    return res.send({ status: 0, message: "invalid username or password" });
+  if (!validPassword) return res.send(notFound);
 
   //generate web token on successful sign in
   let token = user.generateToken();
